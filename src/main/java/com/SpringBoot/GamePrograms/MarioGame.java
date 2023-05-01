9077905c262749a9b96f7e447bef412c2d17c56b
dirisala221 <saikrishna.dirisala@gmail.com>
2023-05-01 00:27:08 -0700

    Mario Game Class.

---
+++ src/main/java/com/SpringBoot/GamePrograms/MarioGame.java
@@ -0,0 +1,17 @@
+package com.SpringBoot.GamePrograms;
+
+public class MarioGame {
+    public void Up(){
+        System.out.println("Jump");
+    }
+    public void Down(){
+        System.out.println("Hide");
+    }
+    public void Right(){
+        System.out.println("Accelerate");
+    }
+    public void Left(){
+        System.out.println("Go back");
+    }
+
+}
