bc2823093ebdb1ec088ff99116c2ea83d924d8b2
dirisala221 <saikrishna.dirisala@gmail.com>
2023-05-01 00:19:20 -0700

    Initial commit

---
+++ src/main/java/com/SpringBoot/PracticePrograms/PracticeProgramsApplication.java
@@ -0,0 +1,13 @@
+package com.SpringBoot.PracticePrograms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeProgramsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeProgramsApplication.class, args);
+	}
+
+}
