242dab75d0b5bf333bd141f1a8dfa933d3de0d71
dirisala221 <saikrishna.dirisala@gmail.com>
2023-05-05 01:01:26 -0700

    Dependencies injection Examples.

--- src/main/java/com/PracticePrograms/InjectionExample1/GameApplicationDependencyInjection1.java
+++ src/main/java/com/PracticePrograms/InjectionExample1/GameApplicationDependencyInjection1.java
@@ -11,10 +11,39 @@ import java.util.Arrays;
 
 @Component
 class BusinessClass{
+    Dependency1 dependency1;
+    Dependency2 dependency2;
+
+    /*
+    This is the constructor Injection. The best thing In the constructor injection is to have the
+     */
+    @Autowired
+    public BusinessClass(Dependency1 dependency1, Dependency2 dependency2) {
+        super();
+        this.dependency1 = dependency1;
+        this.dependency2 = dependency2;
+        System.out.println("Constructor Injection in the Business class.");
+    }
+    /*
+    // These are the Auto wired for the Fields
     @Autowired
     Dependency1 dependency1;
     @Autowired
     Dependency2 dependency2;
+     */
+
+    /*
+    // These are the Autowired for the Setter and Similar code to the getter field.
+    @Autowired
+    public void setDependency1(Dependency1 dependency1) {
+        System.out.println("Setter for: - Set Dependency 1");
+        this.dependency1 = dependency1;
+    }
+    @Autowired
+    public void setDependency2(Dependency2 dependency2) {
+        System.out.println("Setter for: - Set Dependency 2");
+        this.dependency2 = dependency2;
+    } */
 
     public String toString(){
         return "Using " + dependency1 + " and " + dependency2;
